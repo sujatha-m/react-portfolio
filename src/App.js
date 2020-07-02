9fd6862b1a9bf58aaae5cbde5e20df7572df27bb
sujatha <jhanavi.bhushan@gmail.com>
2020-07-02 12:09:03 -0400

    React router for pages links

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header/index'
+//import NavTabs from './components/NavTabs/index'
+import Footer from './components/Footer/index'
+import Home from './components/pages/Home'
+import About from './components/pages/About'
+import Portfolio from "./components/pages/Portfolio"
+import Contact from "./components/pages/Contact"
 
-function App() {
+function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+    <div>
+    <Header/>
+    {/* <NavTabs/> */}
+        <Route exact path ='/' component = {Home} />
+        <Route exact path='/about' component={About} />
+        <Route exact path='/portfolio' component={Portfolio} />
+        <Route path='/contact' component={Contact} /> 
+    
+    <Footer/>
+  </div>
+  </Router>
+  ) 
 }
 
-export default App;
+export default App
