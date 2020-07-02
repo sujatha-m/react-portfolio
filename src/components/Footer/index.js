38237f7937e666b8409369acaff82d72b2b0a602
sujatha <jhanavi.bhushan@gmail.com>
2020-07-02 01:00:52 -0400

    Fixed footer component added

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../../App.css"
+
+function Footer() {
+  return (
+    <section className="copyright">
+    <div className="container">
+        <p>&copy; 2020 copyright all right reserved</p>
+    </div>
+</section>
+  );
+}
+
+export default Footer;
