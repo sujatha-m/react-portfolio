e74de47945a27ae3db6e7b93fa4b40f580697f91
sujatha <jhanavi.bhushan@gmail.com>
2020-07-03 12:25:46 -0400

    Updates

---
+++ precache-manifest.b274bfde0f5daf9c3f6e4b3ec5be4b7a.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d5f1ffbf2e54ef8c393ac4fb5c3acd3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f13ac0c10df887454f6c",
+    "url": "/static/css/main.fb1fda56.chunk.css"
+  },
+  {
+    "revision": "01191d502e87f9c42d35",
+    "url": "/static/js/2.6df5ae3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6df5ae3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f13ac0c10df887454f6c",
+    "url": "/static/js/main.02a800e7.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "a0bd70f87cdfad663a0a823dca2cde1e",
+    "url": "/static/media/EatDaBurger.a0bd70f8.png"
+  },
+  {
+    "revision": "610023e8391858c5e0942deb2b3d45de",
+    "url": "/static/media/EmployeeDirectory.610023e8.png"
+  },
+  {
+    "revision": "a9901cbb359895c0d1606a53a3aafc4c",
+    "url": "/static/media/FitnessTracker.a9901cbb.png"
+  },
+  {
+    "revision": "ea65504cab6b0366b07d1058e2482a33",
+    "url": "/static/media/NoteTaker.ea65504c.png"
+  },
+  {
+    "revision": "9f12d9795c0e681987a417ba2cf27fc2",
+    "url": "/static/media/Project1.9f12d979.png"
+  },
+  {
+    "revision": "43d467b439448e849de765d924fd8ec6",
+    "url": "/static/media/Project2.43d467b4.png"
+  },
+  {
+    "revision": "a568d19edb299dac15b675a2bc8513be",
+    "url": "/static/media/Sujatha_Resume (2).a568d19e.pdf"
+  },
+  {
+    "revision": "ef4ad52b52263bcba9d087cd1cd3c7c7",
+    "url": "/static/media/background.ef4ad52b.jpg"
+  },
+  {
+    "revision": "c27b37cb470347edfa03e76c484294a0",
+    "url": "/static/media/mypic.c27b37cb.jpg"
+  }
+]);
